1ee8b7fdb4baa888fe4a66e53399d8fd3d2da9bb
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2021-07-15 15:17:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var i = 0;
 
 function update() {
     i++;
-    var numbers_of_family_member_in_array = 7;
+    var numbers_of_family_member_in_array = 6;
 
     if (i > numbers_of_family_member_in_array) {
         i = 0;
